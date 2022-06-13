b3bb3e8e963cf1e72da3df237fde171428e85ea1
Erik Uckert <erik.uckert@avl.com>
2022-06-13 10:21:03 +0200

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import HeroPage from './components/HeroPage.vue'
+import Footy from './components/Footy.vue'
+</script>
+
+<template>
+
+  <NavBar></NavBar>
+  <HeroPage></HeroPage>
+  <Footy></Footy>
+
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Footy.vue
@@ -0,0 +1,65 @@
+<script>
+</script>
+<template>
+
+
+<footer class="bg-white sm:p-6 dark:bg-gray-800 max-w-xs sm:max-w-5xl m-10 sm:mx-auto">
+<div class="md:flex md:justify-center mx-auto">
+
+<div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+<div>
+<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+<ul class="text-gray-600 dark:text-gray-400">
+<li class="mb-4">
+<a href="" class="hover:underline">Lorem</a>
+</li>
+<li>
+<a href="https://tailwindcss.com/" class="hover:underline">Porem</a>
+</li>
+</ul>
+</div>
+<div>
+<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+<ul class="text-gray-600 dark:text-gray-400">
+<li class="mb-4">
+<a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
+ </li>
+<li>
+<a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
+</li>
+</ul>
+</div>
+<div>
+<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+<ul class="text-gray-600 dark:text-gray-400">
+<li class="mb-4">
+<a href="#" class="hover:underline">Privacy Policy</a>
+</li>
+<li>
+<a href="#" class="hover:underline">Terms &amp; Conditions</a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
+
+
+<div class="grid grid-cols-2 gap-6 sm:gap-6 sm:grid-cols-4">
+<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+<img src="../assets/logo-bmwi.gif" alt="" class="h-20">
+</a>
+<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+<img src="../assets/logo-eu.jpg" alt="" class="h-20">
+</a>
+<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+<img src="../assets/logo-exist.png" alt="" class="h-20">
+</a>
+<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+<img src="../assets/logo_esf.png" alt="" class="h-20">
+</a>
+
+</div>
+</footer>
+
+</template>
