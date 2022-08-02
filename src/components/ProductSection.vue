e2a2912b07d4035c09b657b407b6fbf894f4d2c8
Erik Uckert <erik.uckert@avl.com>
2022-08-02 12:03:32 +0200

    fix corners for product cards

--- src/components/ProductSection.vue
+++ src/components/ProductSection.vue
@@ -7,10 +7,10 @@
   <section class="mb-32 text-gray-800">
     <div class="container mx-auto xl:px-32 text-center lg:text-left">
       <div class="grid lg:grid-cols-2 flex items-center">
-        <div class="mb-12 lg:mb-0">
-          <div class="block rounded-lg shadow-lg px-6 py-12 md:px-12 lg:-mr-14"
+        <div class="lg:mb-0">
+          <div class="block md:rounded-lg rounded-t-lg shadow-lg px-6 py-12 md:px-12 lg:-mr-14"
             style="background: hsla(0, 0%, 100%, 0.55); backdrop-filter: blur(30px)">
-            <h2 class="text-3xl font-bold mb-6">Printheads</h2>
+            <h3 class="text-2xl font-bold mb-6">Printheads</h3>
             <p class="text-gray-500 mb-0">
     	    We developed a patented printhead that allows you to print our continuous fiber filamentens. 
             Let’s talk about your usecase!
@@ -19,7 +19,7 @@
           </div>
         </div>
         <div>
-          <img src="../assets/printheads.png" class="w-full rounded-lg shadow-lg"
+          <img src="../assets/printheads.png" class="w-full md:rounded-lg rounded-b-lg shadow-lg"
             alt="" />
         </div>
       </div>
@@ -32,13 +32,13 @@
     <div class="container mx-auto xl:px-32 text-center lg:text-left">
       <div class="grid lg:grid-cols-2 flex items-center">
         <div>
-          <img src="../assets/materials.png" class="w-full rounded-lg shadow-lg"
+          <img src="../assets/materials.png" class="w-full md:rounded-lg rounded-t-lg shadow-lg"
             alt="" />
         </div>
         <div class="mb-12 lg:mb-0">
-          <div class="block rounded-lg shadow-lg px-6 py-12 md:px-12 lg:-ml-14"
+          <div class="block md:rounded-lg rounded-b-lg shadow-lg px-6 py-12 md:px-12 lg:-ml-14"
             style="background: hsla(0, 0%, 100%, 0.55); backdrop-filter: blur(30px)">
-            <h2 class="text-3xl font-bold mb-6">Materials</h2>
+            <h3 class="text-2xl font-bold mb-6">Materials</h3>
             <p class="text-gray-500 mb-0">
                 Material development is at the core of our company. 
                 We can deliver a variety of different fiber and polymer combinations. Get in touch to learn more!
