9532f76dac5f4c6369fb6730ddf7d821b6c0eca3
Erik Uckert <erik.uckert@avl.com>
2022-08-10 11:26:49 +0200

    format code and style footy

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -2,37 +2,34 @@
 export default {};
 </script>
 <template>
-<!-- Container for demo purpose -->
-<div class="container my-24 px-6 mx-auto" id="about">
-
-  <!-- Section: Design Block -->
-  <section class="mb-32 text-gray-800 text-center md:text-left">
-    <div class="block rounded-lg shadow-lg bg-white">
-      <div class="flex flex-wrap items-stretch">
-        <div class="basis-auto block lg:flex w-full lg:w-6/12 xl:w-4/12">
-          <img src="../assets/shaft.png" alt="Trendy Pants and Shoes"
-            class="w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg object-cover" />
-        </div>
-        <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-8/12">
-          <div class="px-6 py-12 md:px-12">
-            <h2 class="text-3xl font-bold mb-6 pb-2">About</h2>
-            <p class="text-gray-500 mb-6 pb-2">
-              Endless Industries is a start-up project at TU Berlin, 
-              which successfully received "EXIST Forschungstransfer" funding. 
-              Using the patented core technology, we produce fiber composites using
-              the FDM printing process and are currently developing an exciting
-              product for the orthopedic industry. We want to help
-              manufacturing companies to automate their fiber composite
-              production by providing printing systems and the right material for
-              the job.
-            </p>
+  <div class="container my-24 px-6 mx-auto" id="about">
+    <section class="mb-32 text-gray-800 text-center md:text-left">
+      <div class="block rounded-lg shadow-lg bg-white">
+        <div class="flex flex-wrap items-stretch">
+          <div class="basis-auto block lg:flex w-full lg:w-6/12 xl:w-4/12">
+            <img
+              src="../assets/shaft.png"
+              alt="Trendy Pants and Shoes"
+              class="w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg object-cover"
+            />
+          </div>
+          <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-8/12">
+            <div class="px-6 py-12 md:px-12">
+              <h2 class="text-3xl font-bold mb-6 pb-2">About</h2>
+              <p class="text-gray-500 mb-6 pb-2">
+                Endless Industries is a start-up project at TU Berlin, which
+                successfully received "EXIST Forschungstransfer" funding. Using
+                the patented core technology, we produce fiber composites using
+                the FDM printing process and are currently developing an
+                exciting product for the orthopedic industry. We want to help
+                manufacturing companies to automate their fiber composite
+                production by providing printing systems and the right material
+                for the job.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-  <!-- Section: Design Block -->
-
-</div>
-<!-- Container for demo purpose -->
+    </section>
+  </div>
 </template>
--- src/components/Footy.vue
+++ src/components/Footy.vue
@@ -1,42 +1,54 @@
-<script>
-</script>
+<script></script>
 <template>
+  <div class="container my-24 px-6 mx-auto max-w-2xl" id="footy">
+    <div
+      class="grid grid-cols-2 gap-6 sm:gap-6 sm:grid-cols-4 justify-items-center md:flex md:justify-between"
+    >
+      <a
+        href="#"
+        class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+      >
+        <img src="../assets/logo-bmwi.gif" alt="" class="h-20" />
+      </a>
+      <a
+        href="#"
+        class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+      >
+        <img src="../assets/logo-eu.jpg" alt="" class="h-20" />
+      </a>
+      <a
+        href="#"
+        class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+      >
+        <img src="../assets/logo-exist.png" alt="" class="h-20" />
+      </a>
+      <a
+        href="#"
+        class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+      >
+        <img src="../assets/logo_esf.png" alt="" class="h-20" />
+      </a>
+    </div>
+  </div>
 
-
-
-
-
-<!-- <div class="grid grid-cols-2 gap-6 sm:gap-6 sm:grid-cols-4">
-<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-<img src="../assets/logo-bmwi.gif" alt="" class="h-20">
-</a>
-<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-<img src="../assets/logo-eu.jpg" alt="" class="h-20">
-</a>
-<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-<img src="../assets/logo-exist.png" alt="" class="h-20">
-</a>
-<a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-<img src="../assets/logo_esf.png" alt="" class="h-20">
-</a>
-</div> -->
-
-<div class="container my-24 px-6 mx-auto " id="footy">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="" class="hover:underline">Endless Industries</a>.
-    </span>
-    <br>
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">This website is made with &#9829; by <a href="https://uckert.dev/" class="hover:underline">Erik Uckert</a>.
-    </span>   
-    <ul class="flex justify-center mt-3 text-sm text-gray-500 dark:text-gray-400 mt-20 sm:mt-10">
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Imprint</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy</a>
-        </li>
+  <div class="container my-10 px-6 mx-auto" id="footy">
+    <ul
+      class="flex justify-center text-sm text-gray-500 dark:text-gray-400 my-20 sm:mt-10"
+    >
+      <li>
+        <a href="#" class="mr-4 hover:underline md:mr-6">Imprint</a>
+      </li>
+      <li>
+        <a href="#" class="mr-4 hover:underline md:mr-6">Privacy</a>
+      </li>
     </ul>
-</div>
-
-
-
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+      >© 2022 <a href="" class="hover:underline">Endless Industries</a>
+    </span>
+    <br />
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+      >This website is made with &#9829; by
+      <a href="https://uckert.dev/" class="hover:underline">Erik Uckert</a>.
+    </span>
+  </div>
 </template>
