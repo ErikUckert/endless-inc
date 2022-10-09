9a601198729b180ae9821e3ff8fc325a6875bb11
Erik <uckert@outlook.com>
2022-10-09 12:08:50 +0200

    change script to new vue3 syntax

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,3 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import NavBar from './components/NavBar.vue'
-import HeroPage from './components/HeroPage.vue'
-import Footy from './components/Footy.vue'
-import TeamSection from './components/TeamSection.vue'
-import AboutSection from './components/AboutSection.vue'
-import ProductSection from './components/ProductSection.vue'
-import ContactSection from './components/ContactSection.vue'
-import ContactModal from './components/ContactModal.vue'
-
-</script>
 <script>
 export default {
   data() {
@@ -31,6 +17,20 @@ export default {
   }
   }
 }
+</script>
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import HeroPage from './components/HeroPage.vue'
+import Footy from './components/Footy.vue'
+import TeamSection from './components/TeamSection.vue'
+import AboutSection from './components/AboutSection.vue'
+import ProductSection from './components/ProductSection.vue'
+import ContactSection from './components/ContactSection.vue'
+import ContactModal from './components/ContactModal.vue'
+
 </script>
 
 <template>
