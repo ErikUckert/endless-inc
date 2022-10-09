cbba0f1360396728df473972013c5a0712799a2b
Erik <uckert@outlook.com>
2022-10-09 14:11:09 +0200

    fix vue3 compatibility

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,3 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import NavBar from './components/NavBar.vue'
-import HeroPage from './components/HeroPage.vue'
-import Footy from './components/Footy.vue'
-import TeamSection from './components/TeamSection.vue'
-import AboutSection from './components/AboutSection.vue'
-import ProductSection from './components/ProductSection.vue'
-import ContactSection from './components/ContactSection.vue'
-import ContactModal from './components/ContactModal.vue'
-
-
-
-</script>
-
 <template>
 
   <NavBar></NavBar>
@@ -41,6 +24,50 @@ import ContactModal from './components/ContactModal.vue'
   <TeamSection></TeamSection>
   <Footy></Footy>
 </template>
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import HeroPage from './components/HeroPage.vue'
+import Footy from './components/Footy.vue'
+import TeamSection from './components/TeamSection.vue'
+import AboutSection from './components/AboutSection.vue'
+import ProductSection from './components/ProductSection.vue'
+import ContactSection from './components/ContactSection.vue'
+import ContactModal from './components/ContactModal.vue'
+
+export default {
+    name: 'App',
+    components: {
+    HelloWorld,
+    NavBar,
+    HeroPage,
+    Footy,
+    TeamSection,
+    AboutSection,
+    ProductSection,
+    ContactModal,
+    ContactSection
+  },
+  data() {
+    return {
+        showModal: false,
+        showContactSection: true,
+        contactSectionId: 'contact',
+        contactModalId: 'contactModal'
+    };
+  },
+    methods: {
+    scroll(id) {  
+    document.getElementById(id).scrollIntoView({
+      behavior: "smooth",
+      block: "center"
+    });
+  }
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
